d2b7ad6090ef2c9844aa866ba7ad93e41eaa3337
Lun4t1c <artur.lunat1c.grabowski@gmail.com>
2024-05-17 13:40:39 +0200

    Styling + added game subpages type

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,24 +1,32 @@
 import '../styles/NavBar.css';
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
+import { GameSubPage } from '../utils/types';
 
 function NavBar() {
     const navigate = useNavigate();
-    const [activePage, setActivePage] = useState('');
+    const [activePage, setActivePage] = useState<GameSubPage>('Local');
 
     const navigateTo = (page: string): void => {
         navigate(page);
-        setActivePage(page);
+        switch (page) {
+            case '/':
+                setActivePage('Local');
+                break;
+            case '/ai':
+                setActivePage('AI');
+                break;
+        }
     }
 
     return (
         <div className='navbar'>
 
-            <button className={activePage === '/' ? "navbar-button-active-page" : 'navbar-button'} onClick={() => navigateTo('/')}>
+            <button className={activePage === 'Local' ? "navbar-button-active-page" : 'navbar-button'} onClick={() => navigateTo('/')}>
                 Local
             </button>
 
-            <button className={activePage === '/ai' ? "navbar-button-active-page" : 'navbar-button'} onClick={() => navigateTo('/ai')}>
+            <button className={activePage === 'AI' ? "navbar-button-active-page" : 'navbar-button'} onClick={() => navigateTo('/ai')}>
                 AI
             </button>
 
