20f5354d4a1a8cc707c6bb5ef84bc3a3798d1951
Lun4t1c <artur.lunat1c.grabowski@gmail.com>
2024-05-17 10:43:07 +0200

    Changed page title

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css'
+import './App.css';
 import NavBar from './components/NavBar'
 import GameLocalPage from './components/GameLocalPage';
 import GameAIPage from './components/GameAIPage';
+import DocumentTitle from 'react-document-title';
 
 function App() {
   return (
     <Router>
+      <DocumentTitle title='Tic Tac Toe'></DocumentTitle>
       <NavBar />
       <Routes>
         <Route path="/" element={<GameLocalPage />} />
